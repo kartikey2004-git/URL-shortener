bd7791b647e1e8c9ab756e3d67fc9fba664039fd
kartikey2004-git <kartikeybhatnagar247@gmail.com>
2025-02-02 00:10:16 +0530

    little bit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Button } from "./ui/button";
+import { Button } from "./ui/Button";
 import {
   DropdownMenu,
   DropdownMenuContent,
